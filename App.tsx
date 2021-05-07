e0652b5f35a07d720aff580f4364af90fb32a076
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-05-07 11:17:02 -0700

    WIP main card progress

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { createRef } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 import tailwind from 'tailwind-rn';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { AdMobBanner } from 'expo-ads-admob';
+
 
 export default function App() {
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -12,16 +16,42 @@ export default function App() {
           <Col size={2}>
           </Col>
           <Col size={5}>
-            <Text  style={styles.headerText}>Weather Ride</Text>
+            <Text style={styles.headerText}>Weather Ride</Text>
           </Col>
           <Col size={2}>
           </Col>
         </Grid>
       </View>
       <View style={styles.top}>
-
+        <Card containerStyle={styles.mainCard}>
+          <Card.Title>Motorcycle Day</Card.Title>
+          <Card.Divider />
+          <Grid style={styles.mainCardGrid}>
+            <Col style={styles.temperature} size={60}>
+              <Image
+                style={{ width: 64, height: 64 }}
+                source={require("./assets/sun.png")}
+              />
+            </Col>
+            <Col style={styles.temperature} size={40}>
+              <Text>
+                Today
+                </Text>
+              <Text>
+                37/72
+                </Text>
+            </Col>
+          </Grid>
+        </Card>
       </View>
       <View style={styles.bottom}>
+      </View>
+      <View style={styles.adSection}>
+        <AdMobBanner
+          bannerSize="fullBanner"
+          adUnitID="ca-app-pub-3940256099942544/6300978111"
+          servePersonalizedAds
+        />
       </View>
     </SafeAreaView>
   );
@@ -55,5 +85,31 @@ const styles = StyleSheet.create({
   bottom: {
     height: "60%",
     width: "100%",
+  },
+  mainCard: {
+    height: "100%",
+    borderRadius: 5,
+  },
+  mainCardGrid: {
+    height: "100%",
+  },
+  temperature: {
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    height: 100
+  },
+  adSection: {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    height: 50,
+    width: "100%",
+    backgroundColor: "purple",
+    marginTop: -50
   }
 });
