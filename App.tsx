5fa6bcaa0b5edeb7523121b7aac594d0ca4bdc3c
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-05-07 08:34:14 -0700

    Added header text

--- App.tsx
+++ App.tsx
@@ -1,43 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView , StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import tailwind from 'tailwind-rn';
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-
+        <Grid>
+          <Col size={2}>
+          </Col>
+          <Col size={5}>
+            <Text  style={styles.headerText}>Weather Ride</Text>
+          </Col>
+          <Col size={2}>
+          </Col>
+        </Grid>
       </View>
       <View style={styles.top}>
 
       </View>
       <View style={styles.bottom}>
-
       </View>
-      {/* <Text style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>O654pen up App.tsx to start working on your app! TTT </Text>
-      <StatusBar style="auto" /> */}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%"
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#00d4ff"
   },
   header: {
     height: "10%",
     width: "100%",
-    backgroundColor: "green"
+    display: "flex",
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  headerText: {
+    textAlignVertical: "bottom",
+    height: "100%",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
   },
   top: {
     height: "30%",
     width: "100%",
-    backgroundColor: "blue"
   },
   bottom: {
     height: "60%",
     width: "100%",
-    backgroundColor: "yellow"
   }
 });
